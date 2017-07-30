e136afa1499de1368f70049ecb9770d07b9b2b18
malekan <immalekan@gmail.com>
2017-07-30 11:03:42 +0430

    add new building

--- management/forms.py
+++ management/forms.py
@@ -7,6 +7,14 @@ class BuildingForm(forms.ModelForm):
         model = Building
         fields = ('name', 'address', 'description', 'number_of_floors', 'number_of_elevators')
 
+    def __init__(self, *args, **kwargs):
+        super(BuildingForm, self).__init__(*args, **kwargs)
+        self.fields['name'].label = "نام"
+        self.fields['address'].label = "آدرس"
+        self.fields['description'].label = "توضیح"
+        self.fields['number_of_floors'].label = "تعداد طبقات"
+        self.fields['number_of_elevators'].label = "تعداد آسانسورها"
+
 
 class UnitForm(forms.ModelForm):
     class Meta:
