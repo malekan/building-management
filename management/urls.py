2d846c1e37d736abe35243a83a594b5bd7b9adaa
mohammadmahdi <faryabimohammadmahdi@gmail.com>
2017-08-13 12:56:24 +0430

    added building management panel

--- management/urls.py
+++ management/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^email/$', TemplateView.as_view(template_name='management/email.html')),
     url(r'building/new/$', views.new_building, name='new_building'),
     url(r'building/(?P<building_id>[0-9]+)/edit/', views.BuildingUpdate.as_view(), name='update_building'),
+    url(r'building/management/', TemplateView.as_view(template_name="management/building_management.html")),
     url(r'unit/new/$', views.new_unit, name='new_unit'),
     url(r'building/facility/new', views.new_facility, name='new_facility'),
 ]
